5b3d5cfb461b3c9d1f6637c7b943345dc7f63209
Adrian Razini Rangel <160901383+AdrainRazini@users.noreply.github.com>
2025-08-29 15:17:45 +0000

    Comit

---
+++ Arquivos Py/Tabuada.py
@@ -0,0 +1,13 @@
+Input_String = int(input('Escolha Seu Numero: '))
+
+print('_-_' * 10)
+
+for Numeros in range(1, 11):
+    Contador = Input_String * Numeros
+
+    print(f'{Input_String} x {Numeros} = {Contador}')
+print('_-_' * 10)
+
+
+
+
